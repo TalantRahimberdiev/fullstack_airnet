6f54fe58179728a2c37d7d2123ab74906c6abdea
TalantRahimberdiev <76486068+TalantRahimberdiev@users.noreply.github.com>
2022-11-24 07:27:33 +0300

    Update task.js

--- client_side/src/components/task.js
+++ client_side/src/components/task.js
@@ -12,17 +12,17 @@ export default function Task() {
    const { data, isLoading, error } = useTasksQuery(user.user_id)
 
    return (
-      <div>
+      <>
          <Divider my="lg" color={'dark'} label={<Badge color={'dark'} variant="filled">{user.firstname} {user.lastname} </Badge>} labelPosition="center" />
+      
          <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '5vh' }}>
-
             <Text order={5}><Mark>{time.day}/{time.month}/{time.year}</Mark></Text>
             <Link to={'new_task/'}><Button variant="outline" compact color={'teal'}>new task</Button></Link>
-
          </div>
+
          {
             data && <Tableau tasks={data} />
          }
-      </div>
+      </>
    )
 }
